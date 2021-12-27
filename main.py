d97e905d615ac7b0088c3c20f82e474c95f2c366
Louis Duller <louis.mitchell.duller@gmail.com>
2021-12-27 16:02:56 -0500

    Fixed linting issues

--- main.py
+++ main.py
@@ -1,54 +1,66 @@
-import logging, random
+"""logging for log messages,
+   random for generating a number to guess,
+   sys for exit()"""
+import logging
+import random
+import sys
+
+
 
 class Guess:
-  def check_guess(number, player_guess):
-    if number is player_guess:
-      logging.info(f"CHECK GUESS {number} == {player_guess}")
-      print("You guessed the number!")
-      return True
-    elif number < player_guess:
-      logging.info(f"CHECK GUESS {number} < {player_guess}")
-      print("Your guess is greater than the number.")
-    else:
-      logging.info(f"CHECK GUESS {number} > {player_guess}")
-      print("Your guess is less than the number.")
-    return False
+    """Guess My Number game implementation"""
 
-  def main():
-    # Keep going until user enters negative number
-    while True:
-      # Generate random integer
-      print("Negative inputs will exit.")
-      # Get minimum number for random integer range
-      min = int(input("Enter a minimum number: "))
-      if min < 0:
-        logging.warning("EXITING...")
-        exit()
-      # Get maximum number for random integer range
-      max = int(input("Enter a maximum number: "))
-      while max < 0 or max < min:
-        if max < 0:
-          logging.warning("EXITING...")
-          exit()
+    def check_guess(self, number, player_guess):
+        """Test the player's guess against the pseudo-randomly generated number"""
+        if number is player_guess:
+            logging.info("CHECK GUESS %i == %i", number, player_guess)
+            print("You guessed the number!")
+            return True
+        elif number < player_guess:
+            logging.info("CHECK GUESS %i < %i", number, player_guess)
+            print("Your guess is greater than the number.")
         else:
-          logging.warning("MAXIMUM < MINIMUM.")
-          max = int(input("Enter a new maximum number: "))
-      number = random.randint(min, max)
-      
-      # Get guesses from user
-      guess = int(input("Enter your guess: "))
-      count_guesses = 1
-      # Continue until player guesses correct number
-      while not Guess.check_guess(number, guess):
-        guess = int(input("Enter your new guess: "))
-        count_guesses += 1
-      
+            logging.info("CHECK GUESS %i > %i", number, player_guess)
+            print("Your guess is less than the number.")
+        return False
 
-      logging.info(f"GUESS SUCCESS NUMBER {number} GUESSES {count_guesses}")
-      if count_guesses == 1:
-        print(f"It took {count_guesses} guess to guess {number}.")
-      else:
-        print(f"It took {count_guesses} guesses to guess {number}.")
+    def main(self):
+        """Guess My Number game logic/flow"""
+        # Keep going until user enters negative number
+        while True:
+        # Generate random integer
+            print("Negative inputs will exit.")
+            # Get minimum number for random integer range
+            minimum = int(input("Enter a minimum number: "))
+            if minimum < 0:
+                logging.warning("EXITING...")
+                sys.exit()
+            # Get maximum number for random integer range
+            maximum = int(input("Enter a maximum number: "))
+            while maximum < 0 or maximum < minimum:
+                if maximum < 0:
+                    logging.warning("EXITING...")
+                    sys.exit()
+                else:
+                    logging.warning("MAXIMUM < MINIMUM.")
+                    maximum = int(input("Enter a new maximum number: "))
+            number = random.randint(minimum, maximum)
+
+            # Get guesses from user
+            guess = int(input("Enter your guess: "))
+            count_guesses = 1
+            # Continue until player guesses correct number
+            while not Guess.check_guess(self, number, guess):
+                guess = int(input("Enter your new guess: "))
+                count_guesses += 1
+
+            logging.info("GUESS SUCCESS NUMBER %i GUESSES %i", number, count_guesses)
+            if count_guesses == 1:
+                print(f"It took {count_guesses} guess to guess {number}.")
+            else:
+                print(f"It took {count_guesses} guesses to guess {number}.")
+
+
 
 if __name__ == '__main__':
-  Guess.main()
+    Guess.main(Guess)
--- test.py
+++ test.py
@@ -1,20 +1,25 @@
+"""unittest for unit testing"""
 import unittest
 from main import Guess
 
 #unit testing
 class MyTestCase(unittest.TestCase):
+    """Test Guess.check_guess()"""
 
     def test_guess_correct(self):
+        """Test if guessed number equals generated number"""
         num = 10
         result = Guess.check_guess(num, 10)
         self.assertTrue(result)
 
     def test_guess_incorrect_less(self):
+        """Test if guessed number is less than generated number"""
         num = 25
         result = Guess.check_guess(num, 20)
         self.assertFalse(result)
 
     def test_guess_incorrect_greater(self):
+        """Test if guessed number is greater than generated number"""
         num = 25
         result = Guess.check_guess(num, 100)
         self.assertFalse(result)
